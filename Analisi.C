b3bd3cafd802660d8499244f0d50c3a031a1df29
Andrea <andrew960@hotmail.it>
2020-03-03 18:57:49 +0100

    programma luce aggiunto

--- Analisi.C
+++ Analisi.C
@@ -11,8 +11,8 @@
 void Analisi()
 {
 	//INSERIRE n=MINIMO NUMERO RIGHE DEI 3 FILE
-	int n=483;
-	int i;
+	int n=400;
+	double l=270;//cm
 	//array per vedere determinare dt1 e dt2 in due modi diversi, a e b
 	double dt1a[n];
 	double dt1b[n];
@@ -21,30 +21,35 @@ void Analisi()
 	//array che rappresentano la differenza tra i 2 metodi a e b di determinazione dei tempi
 	double dt1[n];
 	double dt2[n];
+	double ttot[n];
+	//array della velocità della luce nella sbarra
+	double v[n];
 	//istogrammi dei tempi nelle configurazioni paletta a sx (_s), a dx(_d) e al centro(_c)
-	TH1D* h1_s =  new TH1D("h1_s", "Tempo_s",200 ,0,4);
-	TH1D* h2_s =  new TH1D("h2_s", "Pmt 1_s",200 ,0,4);
-	TH1D* h3_s =  new TH1D("h3_s", "Pmt 2_s",200 ,0,4);	
-	TH1D* h1_c =  new TH1D("h1_c", "Tempo_c",200 ,0,4);
-	TH1D* h2_c =  new TH1D("h2_c", "Pmt 1_c",200 ,0,4);
-	TH1D* h3_c =  new TH1D("h3_c", "Pmt 2_c",200 ,0,4);	
-	TH1D* h1_d =  new TH1D("h1_d", "Tempo_d",200 ,0,4);
-	TH1D* h2_d =  new TH1D("h2_d", "Pmt 1_d",200 ,0,4);
-	TH1D* h3_d =  new TH1D("h3_d", "Pmt 2_d",200 ,0,4);
-	//istogrammi per determinare le differenze dei tempi dt1 e dt2, cioè PMT1-centro e PMT2-centro	
-	TH1D* ht1 =  new TH1D("ht1", "dt1a-dt1b",200 ,0,4);	
-	TH1D* ht2 =  new TH1D("ht2", "dt2a-dt2b",200 ,0,4);	
+	TH1D* h1_s =  new TH1D("h1_s", "Tempo_s",200 ,0,0);
+	TH1D* h2_s =  new TH1D("h2_s", "Pmt 1_s",200 ,0,0);
+	TH1D* h3_s =  new TH1D("h3_s", "Pmt 2_s",200 ,0,0);	
+	TH1D* h1_c =  new TH1D("h1_c", "Tempo_c",200 ,0,0);
+	TH1D* h2_c =  new TH1D("h2_c", "Pmt 1_c",200 ,0,0);
+	TH1D* h3_c =  new TH1D("h3_c", "Pmt 2_c",200 ,0,0);	
+	TH1D* h1_d =  new TH1D("h1_d", "Tempo_d",200 ,0,0);
+	TH1D* h2_d =  new TH1D("h2_d", "Pmt 1_d",200 ,0,0);
+	TH1D* h3_d =  new TH1D("h3_d", "Pmt 2_d",200 ,0,0);
 	TCanvas* c1= new TCanvas("c1", "Singoli tempi nelle 3 configurazioni della paletta", 2000,500);
+	//istogrammi per determinare le differenze dei tempi dt1 e dt2, cioè PMT1-centro e PMT2-centro	
+	TH1D* ht1 =  new TH1D("ht1", "dt1a-dt1b",200 ,0,0);	
+	TH1D* ht2 =  new TH1D("ht2", "dt2a-dt2b",200 ,0,0);	
 	TCanvas* c2= new TCanvas("c2", "Differenze temporali", 2000,500);
-
+	//istogramma per la velocita' della luce
+	TH1D* hv =  new TH1D("hv", "Velocita' luce nella sbarra",200 ,0,0);
+	TCanvas* c3= new TCanvas("c3", "Velocità luce nella sbarra", 2000,500);
 
 	//Legge il file e crea 9 vector n dimensionali    	
 	std::vector<double> Time_s(n), Pmt1_s(n), Pmt2_s(n);
-	std::ifstream fs_read("2602s.dat");
+	std::ifstream fs_read("luces2802.dat");
 	std::vector<double> Time_c(n), Pmt1_c(n), Pmt2_c(n);
-	std::ifstream fc_read("2602c.dat");
+	std::ifstream fc_read("2802c.dat");
 	std::vector<double> Time_d(n), Pmt1_d(n), Pmt2_d(n);
-	std::ifstream fd_read("2602d.dat");
+	std::ifstream fd_read("luced0203.dat");
 	//Riempio i vector
     	for(int i = 0; i < n; i++)
     	{
@@ -52,7 +57,20 @@ void Analisi()
 		fc_read >> Time_c[i] >> Pmt1_c[i] >> Pmt2_c[i];
 		fd_read >> Time_d[i] >> Pmt1_d[i] >> Pmt2_d[i];
     	}
-  	//determino i due intervalli temporali (sx-centro, dx-centro) nei modi a e b
+    	//conversioni in secondi
+    	for(int i = 0; i < n; i++)
+    	{
+    		Pmt1_s[i]=Pmt1_s[i]*40;
+    		Pmt2_s[i]=Pmt2_s[i]*40;
+    		Pmt1_c[i]=Pmt1_c[i]*20;
+    		Pmt2_c[i]=Pmt2_c[i]*20;
+    		Pmt1_d[i]=Pmt1_d[i]*40;
+    		Pmt2_d[i]=Pmt2_d[i]*40;
+    		
+    		
+    	}
+    	
+    	//determino i due intervalli temporali (sx-centro, dx-centro) nei modi a e b
 	for(int i = 0; i < n; i++)
     	{
     		//(paletta a sx,tempo PMT a dx)-(paletta al centro,tempo PMT a dx)
@@ -70,6 +88,24 @@ void Analisi()
     		dt1[i]=dt1a[i]-dt1b[i];
     		dt2[i]=dt2a[i]-dt2b[i];
     	}
+    	//calcolo il tempo totale di attraversamento
+    	for(int i = 0; i < n; i++)
+    	{
+    		ttot[i]=(Pmt2_s[i]-Pmt2_d[i]);
+    		
+    	}
+    	
+    	//calcolo la velocità della luce nella sbarra ATTENZIONE ALLE CONDIZIONI DI ESISTENZA
+    	for(int i = 0; i < n; i++)
+    	{
+    		v[i]=l/ttot[i];
+    	}
+    	
+    	for(int i = 0; i < n; i++)
+    	{
+    		hv->Fill(v[i]);
+    	}
+    	
 	//riempio istogrammi per le differenze temporali nei modi a e b dei due intervalli (sx-centro, dx-centro)
 	for(int j=0; j<n; j++)
 	{
@@ -107,6 +143,9 @@ void Analisi()
   	ht1->Draw();
   	c2->cd(2);
   	ht2->Draw();
-  	
-  
+	c3->cd();
+	hv->Draw();  	
+	
+	
+	
 }
